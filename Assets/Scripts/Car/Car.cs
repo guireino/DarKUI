c2891232f3976719908684255c5c3f9de44beb76
Guilherme A. R <guiabucarma@gmail.com>
2024-05-27 11:52:44 -0400

    add script Car

---
+++ Assets/Scripts/Car/Car.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour{
+
+    public GameObject carPrefab;
+
+    public int speed = 20, gear = 5;
+
+    public int TotalSpeed{
+        get { return speed * gear; }
+    }
+    
+    public void CreateCar(){
+
+        var a = Instantiate(carPrefab);
+        a.transform.position = Vector3.zero;
+    }
+}
